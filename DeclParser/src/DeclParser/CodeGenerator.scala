96c3b7debd80cef637b233688de071643adb70ee
vasily-knk <vasily.knk@gmail.com>
2014-04-18 03:03:07 +0400

    CPP generation

--- DeclParser/src/DeclParser/CodeGenerator.scala
+++ DeclParser/src/DeclParser/CodeGenerator.scala
@@ -32,6 +32,24 @@ object CodeGenerator {
             (ns) => "} // namespace " + ns + "\r\n\r\n"
         ).mkString("")
     }
+
+    private def fixUnnamedArgs(args: Args) : Args = {
+        var counter: Int = 0
+
+        def newName(): String = {
+            val res = "unnamed" + counter.toString
+            counter += 1
+            res
+        }
+
+        args.map(
+            (arg) => Arg(arg.argType, arg.name match {
+                case Some(s) => Some(s)
+                case None => Some(newName())
+            })
+        )
+    }
+
 }
 
 class CodeGenerator extends InterfacesProcessor {
@@ -40,13 +58,12 @@ class CodeGenerator extends InterfacesProcessor {
 
     private def writeHeader(i: Interface) {
         val name = stripName(i.name)
-
+        val namespaces = baseNamespaces :+ name
         val pw = new PW(proxyFilename(name) + ".h")
 
         pw.println("#pragma once")
         pw.println()
 
-        val namespaces = baseNamespaces :+ name
 
         pw.println(openNamespaces(namespaces))
 
@@ -59,9 +76,7 @@ class CodeGenerator extends InterfacesProcessor {
             """struct ProxyBase
               |    : IBase
               |{
-              |    ProxyBase(IBasePtr pimpl)
-              |        : pimpl_(pimpl)
-              |    {}
+              |    ProxyBase(IBasePtr pimpl);
             """.stripMargin)
 
 
@@ -91,8 +106,61 @@ class CodeGenerator extends InterfacesProcessor {
         pw.close()
     }
 
+    private def writeCPP(i: Interface) {
+        val name = stripName(i.name)
+        val namespaces = baseNamespaces :+ name
+        val pw = new PW(proxyFilename(name) + ".cpp")
+
+        val headerName = proxyFilename(name) + ".h"
+        pw.println("#include \"%s\"".format(headerName))
+        pw.println()
+
+        pw.println(openNamespaces(namespaces))
+
+        pw.println("ProxyBase::ProxyBase(IBasePtr *)")
+        pw.println("    : pimpl_(pimpl)")
+        pw.println("{")
+        pw.println("}")
+        pw.println()
+
+        i.methods.foreach((m) => {
+            val args = fixUnnamedArgs(m.args)
+
+            pw.print(m.retType + " " + "ProxyBase::" + m.name + "(")
+
+            val argStrings = args.map((arg) => arg.argType + (arg.name match {
+                case Some(name) => " " + name
+            }))
+
+            pw.print(argStrings.mkString(", "))
+            pw.println(")")
+            pw.println("{")
+
+            pw.print("    ")
+            if (m.retType != "void")
+                pw.print("return ")
+
+            pw.print("pimpl_->" + m.name + "(")
+            val argStringsOnlyNames = args.map((arg) => arg.name match {
+                case Some(name) => name
+            })
+
+            pw.print(argStringsOnlyNames.mkString(", "))
+            pw.println(");")
+
+            pw.println("}")
+            pw.println()
+        })
+
+
+        pw.println(closeNamespaces(namespaces))
+
+        pw.close()
+    }
+
     private def processInterface(i: Interface) {
         writeHeader(i)
+        writeCPP(i)
     }
 
     def process(interfaces: Interfaces) {
